5651b84570fc62f7aac7936be1e4ac0116b5d758
Mustapha Elzehary <mustapha.elzehary@gmail.com>
2022-03-07 00:54:01 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="css/reset-min.css" rel="stylesheet"/>
+        <link href="css/fonts-min.css" rel="stylesheet"/>
+        <link href="css/app.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="app-container">
+            <div id="app-wrapper">
+                <div class="header-wrapper">
+                    <div class="header-content">
+                        <div class="flx-cnt">
+                            <lable class="btn-1x txt-orange txt-bold fnt-sz-2x bg-orange bx-rond-1x">pricing</lable>
+                        </div>
+                        <div class="flx-cnt">
+                            <label class="txt-bold txt-cnt fnt-sz-3x">No Hidden charges! choose <br> Your best plan.</label>
+                        </div>
+                        <div class="flx-cnt">
+                            <span class="txt-cnt fnt-sz-1x">
+                                samams Travaling with Mustapha in happy journy to the end of the seen versi any text is writen here :D 
+                                samams Travaling with Mustapha in happy journy to the end of the seen versi any text is writen here :D
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="content-wrapper">
+                    <div class="content-body">
+                        <ul class="plan-list">
+                            <li class="unit-plan">
+                                <div class="plan-card-wrapper">
+                                    <div class="plan-card-body">
+                                        <div class="plan-name-wapper flx-cnt">
+                                            <span class="fnt-sz-4x">Premuim Plan</span>
+                                        </div>
+                                        <div class="plan-icon-wrapper flx-cnt">
+                                            <i class="fa fa-envelope-open-o fnt-sz-8x" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="plan-price-wrapper">
+                                            <div class="price"> 
+                                                <i class="fa fa-usd fa-3x" aria-hidden="true"></i>
+                                                <span class="fnt-sz-8x txt-bold">125</span>
+                                            </div>
+                                            <div class="flx-cnt">
+                                                <span class="fnt-sz-4x">PER MONTH</span>
+                                            </div>
+                                        </div>
+                                        <div class="plan-feature">
+                                            <ul>
+                                                <li class="txt-cnt">Finance Business Test</li>
+                                                <li class="txt-cnt">Finance Business Test Test Test Again</li>
+                                                <li class="txt-cnt">Finance Business -- </li>
+                                                <li class="txt-cnt">Finance Business Trait</li>
+                                            </ul>
+                                        </div>
+                                        <div class="plan-select-wrapper flx-cnt">
+                                            <button class="btn bx-rond-2x">Select Plane</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="unit-plan">
+                                <div class="plan-card-wrapper">
+                                    <div class="plan-card-body">
+                                        <div class="plan-name-wapper flx-cnt">
+                                            <span class="fnt-sz-4x">Premuim Plan</span>
+                                        </div>
+                                        <div class="plan-icon-wrapper flx-cnt">
+                                            <i class="fa fa-envelope-open-o fnt-sz-8x" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="plan-price-wrapper">
+                                            <div class="price"> 
+                                                <i class="fa fa-usd fa-3x" aria-hidden="true"></i>
+                                                <span class="fnt-sz-8x txt-bold">125</span>
+                                            </div>
+                                            <div class="flx-cnt">
+                                                <span class="fnt-sz-4x">PER MONTH</span>
+                                            </div>
+                                        </div>
+                                        <div class="plan-feature">
+                                            <ul>
+                                                <li class="txt-cnt">Finance Business Test</li>
+                                                <li class="txt-cnt">Finance Business Test Test Test Again</li>
+                                                <li class="txt-cnt">Finance Business -- </li>
+                                                <li class="txt-cnt">Finance Business Trait</li>
+                                            </ul>
+                                        </div>
+                                        <div class="plan-select-wrapper flx-cnt">
+                                            <button class="btn bx-rond-2x">Select Plane</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="unit-plan">
+                                <div class="plan-card-wrapper">
+                                    <div class="plan-card-body">
+                                        <div class="plan-name-wapper flx-cnt">
+                                            <span class="fnt-sz-4x">Premuim Plan</span>
+                                        </div>
+                                        <div class="plan-icon-wrapper flx-cnt">
+                                            <i class="fa fa-envelope-open-o fnt-sz-8x" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="plan-price-wrapper">
+                                            <div class="price"> 
+                                                <i class="fa fa-usd fa-3x" aria-hidden="true"></i>
+                                                <span class="fnt-sz-8x txt-bold">125</span>
+                                            </div>
+                                            <div class="flx-cnt">
+                                                <span class="fnt-sz-4x">PER MONTH</span>
+                                            </div>
+                                        </div>
+                                        <div class="plan-feature">
+                                            <ul>
+                                                <li class="txt-cnt">Finance Business Test</li>
+                                                <li class="txt-cnt">Finance Business Test Test Test Again</li>
+                                                <li class="txt-cnt">Finance Business -- </li>
+                                                <li class="txt-cnt">Finance Business Trait</li>
+                                            </ul>
+                                        </div>
+                                        <div class="plan-select-wrapper flx-cnt">
+                                            <button class="btn bx-rond-2x">Select Plane</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="sponser-list-wrapper">
+                            <div class="sponser-list-body">
+                                <ul>
+                                    <li class="">
+                                        <div class="sponser-unit flx-cnt">
+                                            <div class="sponser-logo">
+                                                <img src="https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png"/>
+                                            </div>
+                                            <div class="sponser-title">
+                                                <div class="name fnt-sz-4x txt-bold">Egol Glows</div>
+                                                <div class="name-for fnt-sz-1x"> The Birdes Lar</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="">
+                                        <div class="sponser-unit flx-cnt">
+                                            <div class="sponser-logo">
+                                                <img src="https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png"/>
+                                            </div>
+                                            <div class="sponser-title">
+                                                <div class="name fnt-sz-4x txt-bold">Egol Glows</div>
+                                                <div class="name-for fnt-sz-1x"> The Birdes Lar</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="">
+                                        <div class="sponser-unit flx-cnt">
+                                            <div class="sponser-logo">
+                                                <img src="https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png"/>
+                                            </div>
+                                            <div class="sponser-title">
+                                                <div class="name fnt-sz-4x txt-bold">Egol Glows</div>
+                                                <div class="name-for fnt-sz-1x"> The Birdes Lar</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="">
+                                        <div class="sponser-unit flx-cnt">
+                                            <div class="sponser-logo">
+                                                <img src="https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png"/>
+                                            </div>
+                                            <div class="sponser-title">
+                                                <div class="name fnt-sz-4x txt-bold">Egol Glows</div>
+                                                <div class="name-for fnt-sz-1x"> The Birdes Lar</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="">
+                                        <div class="sponser-unit flx-cnt">
+                                            <div class="sponser-logo">
+                                                <img src="https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png"/>
+                                            </div>
+                                            <div class="sponser-title">
+                                                <div class="name fnt-sz-4x txt-bold">Egol Glows</div>
+                                                <div class="name-for fnt-sz-1x"> The Birdes Lar</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="testim-wrapper">
+                            <div class="testim-body">
+                                <div class="testim-image-wapper part"></div>
+                                <div class="testim-quot-wrapper part">
+                                    <div class="bx-margin-3x">
+                                        <lable class="btn-1x txt-orange txt-bold fnt-sz-2x bg-orange bx-rond-1x">Long Word Long Word</lable>
+                                    </div>
+                                    <div class="bx-margin-1x">
+                                       <label class="txt-bold txt-cnt fnt-sz-3x">No Hidden charges! choose Your best plan.</label>
+                                    </div>
+                                    <div class=" bx-margin-1x fnt-sz-1x">
+                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                            <span>samams Travaling with Mustapha in happy journy to the end of the 
+                                                seen versi any text is writen here :D 
+                                            samams Travaling with Mustapha in happy journy to the end of the seen versi any text
+                                            is writen here :D'Annunzio</span>
+                                            <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="person-testim-wrapper bx-margin-2x">
+                                        <div class="person-name txt-bold fnt-sz-2x  bx-margin-1x">Mustapha Elzehary</div>
+                                        <div class="person-testim fnt-sz-1x">This my testimonie To this company</div>
+                                    </div>
+                                    <div class="dots">
+                                        <div class="dot bg-orange"></div>
+                                        <div class="dot bg-orange"></div>
+                                        <div class="dot bg-orange"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-wrapper">
+                <div class="footer-body">
+                    <div class="footer-content">
+                        <div></div>
+                        <div class="col grid-4x">
+                            <div class="title fnt-sz-3x txt-bold"> Company</div>
+                            <div>
+                                <ul class="flx-cnt">
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
