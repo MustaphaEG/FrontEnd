4df49ee162fe32427f68e5975c68c3a64653d898
Mustapha Elzehary <mustapha.elzehary@gmail.com>
2022-03-07 09:30:44 +0200

    Trait

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <div id="app-container">
             <div id="app-wrapper">
+                <div class="circle"></div>
                 <div class="header-wrapper">
                     <div class="header-content">
                         <div class="flx-cnt">
@@ -202,15 +203,15 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                                         <lable class="btn-1x txt-orange txt-bold fnt-sz-2x bg-orange bx-rond-1x">Long Word Long Word</lable>
                                     </div>
                                     <div class="bx-margin-1x">
-                                       <label class="txt-bold txt-cnt fnt-sz-3x">No Hidden charges! choose Your best plan.</label>
+                                        <label class="txt-bold txt-cnt fnt-sz-3x">No Hidden charges! choose Your best plan.</label>
                                     </div>
                                     <div class=" bx-margin-1x fnt-sz-1x">
-                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                                            <span>samams Travaling with Mustapha in happy journy to the end of the 
-                                                seen versi any text is writen here :D 
+                                        <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                        <span>samams Travaling with Mustapha in happy journy to the end of the 
+                                            seen versi any text is writen here :D 
                                             samams Travaling with Mustapha in happy journy to the end of the seen versi any text
                                             is writen here :D'Annunzio</span>
-                                            <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
                                     </div>
                                     <div class="person-testim-wrapper bx-margin-2x">
                                         <div class="person-name txt-bold fnt-sz-2x  bx-margin-1x">Mustapha Elzehary</div>
@@ -230,22 +231,70 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             <div class="footer-wrapper">
                 <div class="footer-body">
                     <div class="footer-content">
-                        <div></div>
                         <div class="col grid-4x">
-                            <div class="title fnt-sz-3x txt-bold"> Company</div>
+                            <div class="title fnt-sz-3x txt-bold bx-margin-2x"> SEOUL .</div>
                             <div>
-                                <ul class="flx-cnt">
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
-                                    <li class="grid-2x"><a class="txt-white" href="#">About us</a></li>
+                                <p class="bx-margin-1x txt-white">Street Street Streeedt</p>
+                                <p class="bx-margin-1x txt-white">Street Street Streeedt</p>
+                            </div>
+                            <div class="social-list">
+                                <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                                <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                                <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                                <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                                <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <div class="col grid-4x">
+                            <div class="title fnt-sz-3x txt-bold bx-margin-2x"> Company</div>
+                            <div>
+                                <ul class="link-list">
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-2x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div></div>
-                        <div></div>
+                        <div class="col grid-4x">
+                            <div class="title fnt-sz-3x txt-bold bx-margin-2x"> Services</div>
+                            <div>
+                                <ul class="link-list">
+                                    <li class="grid-1x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-1x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-1x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                    <li class="grid-1x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                         <div class="col grid-4x">
+                            <div class="title fnt-sz-3x txt-bold bx-margin-2x"> News Letter</div>
+                            <div>
+                                <p class="bx-margin-1x txt-white">Street Street Streeedt</p>
+                                <p class="bx-margin-1x txt-white">Street Street Streeedt</p>
+                            </div>
+                            <div class="social-list bx-margin-2x">
+                                <button class="email-adress">
+                                    Email Address : 
+                                    <i class="fa fa-facebook-square fa-2x txt-white" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="copy-right">
+                        <div class="txt-white  grid-2x">Copy right 132453</div>
+                        <div class="grid-4x">
+                            <ul>
+                                <li class="grid-4x bx-margin-1x"><a class="txt-white" href="#">About us  </a></li>
+                                <li class="grid-4x bx-margin-1x"><a class="txt-white" href="#">About us  </a></li>
+                                <li class="grid-4x bx-margin-1x"><a class="txt-white" href="#">About us  </a></li>
+                                <li class="grid-4x bx-margin-1x"><a class="txt-white" href="#">About us</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
